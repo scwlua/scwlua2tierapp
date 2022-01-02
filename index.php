13f8599f5bc3a834af260fb80a7616e9475627cc
Shawn Lua <shawn.lua.cw@gmail.com>
2022-01-02 12:15:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <title>Spoon-Knife</title>
+  <title>none</title>
   <LINK href="styles.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
-<img src="forkit.gif" id="octocat" alt="" />
-
-<!-- Feel free to change this text here -->
 <p>
  Thanks for attending this demo!
 </p>
@@ -17,13 +14,12 @@
 <br>
 
 <p>
- Click on the button to visit the entry page.
+ Click on the button to visit the Record Entry Application.
 </p>
 
-<!-- need to change to the path.txt -->
 <a href="SamplePage.php">
-  <button>SamplePageSimple</button>
-</a> 
+  <button>Record Entry Application</button>
+</a>
 
 </body>
 </html>
